82a78d447f5a30f8133dd23baf5468338006fe62
JAVIER\menaj <menajavier2002@gmail.com>
2023-01-11 10:00:57 +0100

    añadido ng fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TareaListaComponent } from './tarea-lista/tarea-lista.component';
 import { EdiTareaComponent } from './edi-tarea/edi-tarea.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { EdiTareaComponent } from './edi-tarea/edi-tarea.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
